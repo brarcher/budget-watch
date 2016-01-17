975ab4c2ddccdfc72bbfadaa3d1892d73828dc2d
Branden Archer <b.m.archer4@gmail.com>
2016-01-17 14:18:17 -0500

    rename activity_main to main_activity
    it will be easier to determine what layouts pertain to what
    activities.

--- app/src/main/java/protect/budgetwatch/MainActivity.java
+++ app/src/main/java/protect/budgetwatch/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
