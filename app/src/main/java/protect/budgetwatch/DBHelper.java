1db3181866c4f8a388aa290aab9fe946ec8af67f
Branden Archer <b.m.archer4@gmail.com>
2016-01-23 21:57:14 -0500

    Add support for transactions in the database

--- app/src/main/java/protect/budgetwatch/DBHelper.java
+++ app/src/main/java/protect/budgetwatch/DBHelper.java
@@ -21,6 +21,22 @@ public class DBHelper extends SQLiteOpenHelper
         public static final String MAX = "max";
     }
 
+    class TransactionDbIds
+    {
+        public static final String TABLE = "transactions";
+        public static final String NAME = "_id";
+        public static final String TYPE = "type";
+        public static final String DESCRIPTION = "description";
+        public static final String ACCOUNT = "account";
+        public static final String BUDGET = "budget";
+        public static final String VALUE = "value";
+        public static final String NOTE = "note";
+        public static final String DATE = "date";
+
+        public static final int EXPENSE = 1;
+        public static final int REVENUE = 2;
+    }
+
     public DBHelper(Context context)
     {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -34,6 +50,16 @@ public class DBHelper extends SQLiteOpenHelper
             "create table  " + BudgetDbIds.TABLE + "(" +
                 BudgetDbIds.NAME + " text primary key," +
                 BudgetDbIds.MAX + " INTEGER not null)");
+       // create table for transactions
+        db.execSQL("create table " + TransactionDbIds.TABLE + "(" +
+                TransactionDbIds.NAME + " INTEGER primary key autoincrement," +
+                TransactionDbIds.TYPE + " INTEGER not null," +
+                TransactionDbIds.DESCRIPTION + " TEXT not null," +
+                TransactionDbIds.ACCOUNT + " TEXT," +
+                TransactionDbIds.BUDGET + " TEXT," +
+                TransactionDbIds.VALUE + " REAL not null," +
+                TransactionDbIds.NOTE + " TEXT," +
+                TransactionDbIds.DATE + " INTEGER not null)");
     }
 
     @Override
@@ -41,6 +67,7 @@ public class DBHelper extends SQLiteOpenHelper
     {
         // Do not support versioning yet
         db.execSQL("DROP TABLE IF EXISTS " + BudgetDbIds.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + TransactionDbIds.TABLE);
         onCreate(db);
     }
 
@@ -73,6 +100,27 @@ public class DBHelper extends SQLiteOpenHelper
         return (rowsDeleted == 1);
     }
 
+    public Budget getBudget(final String name)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor data = db.rawQuery("select * from " + BudgetDbIds.TABLE +
+                " where " + BudgetDbIds.NAME + "=?", new String[]{name});
+
+        Budget budget = null;
+
+        if(data.getCount() == 1)
+        {
+            data.moveToFirst();
+            String goalName = data.getString(data.getColumnIndexOrThrow(BudgetDbIds.NAME));
+            int goalMax = data.getInt(data.getColumnIndexOrThrow(BudgetDbIds.MAX));
+
+            budget = new Budget(goalName, goalMax, 0);
+        }
+
+        data.close();
+
+        return budget;
+    }
 
     public List<Budget> getBudgets(long startDateMs, long endDateMs)
     {
@@ -87,9 +135,7 @@ public class DBHelper extends SQLiteOpenHelper
             {
                 String name = data.getString(data.getColumnIndexOrThrow(BudgetDbIds.NAME));
                 int max = data.getInt(data.getColumnIndexOrThrow(BudgetDbIds.MAX));
-
-                // STOPSHIP: Get actual value
-                int current = 0;
+                int current = getTotalForBudget(name, startDateMs, endDateMs);
 
                 budgets.add(new Budget(name, max, current));
             } while(data.moveToNext());
@@ -98,6 +144,32 @@ public class DBHelper extends SQLiteOpenHelper
         data.close();
 
         return budgets;
+    }
+
+    public int getTotalForBudget(String budget, long startDateMs, long endDateMs)
+    {
+        int expense = getTotalForTransactionType(TransactionDbIds.EXPENSE, budget, startDateMs, endDateMs);
+        int revenue = getTotalForTransactionType(TransactionDbIds.REVENUE, budget, startDateMs, endDateMs);
+
+        return expense - revenue;
+    }
+
+    public int getTotalForTransactionType(int type, String budget, long startDateMs, long endDateMs)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor data = db.rawQuery("select total(" + TransactionDbIds.VALUE + ") " +
+                        "from " + TransactionDbIds.TABLE + " " +
+                        "WHERE " + TransactionDbIds.BUDGET + " = ? AND " +
+                        TransactionDbIds.TYPE + " = ? AND " +
+                        TransactionDbIds.DATE + " >= ? AND " +
+                        TransactionDbIds.DATE + " <= ?",
+                new String[]{budget, Integer.toString(type),
+                        Long.toString(startDateMs), Long.toString(endDateMs)});
+        data.moveToFirst();
+        int value = data.getInt(0);
+        data.close();
+
+        return value;
     }
 
     public List<String> getBudgetNames()
@@ -120,5 +192,86 @@ public class DBHelper extends SQLiteOpenHelper
         data.close();
 
         return budgetNames;
+    }
+
+    public boolean insertTransaction(final int type, final String description, final String account, final String budget,
+                                 final double value, final String note, final long dateInMs)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TransactionDbIds.TYPE, type);
+        contentValues.put(TransactionDbIds.DESCRIPTION, description);
+        contentValues.put(TransactionDbIds.ACCOUNT, account);
+        contentValues.put(TransactionDbIds.BUDGET, budget);
+        contentValues.put(TransactionDbIds.VALUE, value);
+        contentValues.put(TransactionDbIds.NOTE, note);
+        contentValues.put(TransactionDbIds.DATE, dateInMs);
+
+        long newId = db.insert(TransactionDbIds.TABLE, null, contentValues);
+        return (newId != 1);
+    }
+
+    public boolean updateTransaction(final int id, final int type, final String description,
+                                     final String account, final String budget, final double value,
+                                     final String note, final long dateInMs)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TransactionDbIds.TYPE, type);
+        contentValues.put(TransactionDbIds.DESCRIPTION, description);
+        contentValues.put(TransactionDbIds.ACCOUNT, account);
+        contentValues.put(TransactionDbIds.BUDGET, budget);
+        contentValues.put(TransactionDbIds.VALUE, value);
+        contentValues.put(TransactionDbIds.NOTE, note);
+        contentValues.put(TransactionDbIds.DATE, dateInMs);
+        int rowsUpdated = db.update(TransactionDbIds.TABLE, contentValues, TransactionDbIds.NAME + "=?",
+                new String[]{Integer.toString(id)});
+        return (rowsUpdated == 1);
+    }
+
+    public Transaction getTransaction(final int id)
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor data = db.rawQuery("select * from " + TransactionDbIds.TABLE +
+                " where " + TransactionDbIds.NAME + "=?", new String[]{String.format("%d", id)});
+
+        Transaction transaction = null;
+
+        if(data.getCount() == 1)
+        {
+            data.moveToFirst();
+            transaction = Transaction.toTransaction(data);
+        }
+
+        data.close();
+
+        return transaction;
+    }
+
+    public boolean deleteTransaction(final int id)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        int rowsDeleted =  db.delete(TransactionDbIds.TABLE,
+                TransactionDbIds.NAME + " = ? ",
+                new String[]{Integer.valueOf(id).toString()});
+        return (rowsDeleted == 1);
+    }
+
+    public Cursor getExpenses()
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor res =  db.rawQuery("select * from " + TransactionDbIds.TABLE + " where " +
+                TransactionDbIds.TYPE + "=" + TransactionDbIds.EXPENSE +
+                " ORDER BY " + TransactionDbIds.DATE + " DESC", null);
+        return res;
+    }
+
+    public Cursor getRevenues()
+    {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor res =  db.rawQuery("select * from " + TransactionDbIds.TABLE + " where " +
+                TransactionDbIds.TYPE + "=" + TransactionDbIds.REVENUE +
+                " ORDER BY " + TransactionDbIds.DATE + " DESC", null);
+        return res;
     }
 }
---
+++ app/src/main/java/protect/budgetwatch/Transaction.java
@@ -0,0 +1,42 @@
+package protect.budgetwatch;
+
+import android.database.Cursor;
+
+public class Transaction
+{
+    public final int id;
+    public final int type;
+    public final String description;
+    public final String account;
+    public final String budget;
+    public final double value;
+    public final String note;
+    public final long dateMs;
+
+    public Transaction(final int id, final int type, final String description, final String account,
+                       final String budget, final double value, final String note, final long dateMs)
+    {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+        this.account = account;
+        this.budget = budget;
+        this.value = value;
+        this.note = note;
+        this.dateMs = dateMs;
+    }
+
+    public static Transaction toTransaction(Cursor cursor)
+    {
+        int id = cursor.getInt(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.NAME));
+        int type = cursor.getInt(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.TYPE));
+        String description = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.DESCRIPTION));
+        String account = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.ACCOUNT));
+        String budget = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.BUDGET));
+        double value = cursor.getDouble(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.VALUE));
+        String note = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.NOTE));
+        long dateMs = cursor.getLong(cursor.getColumnIndexOrThrow(DBHelper.TransactionDbIds.DATE));
+
+        return new Transaction(id, type, description, account, budget, value, note, dateMs);
+    }
+}
