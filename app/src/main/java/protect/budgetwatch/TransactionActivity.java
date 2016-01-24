9c6d2b9a74dff9ac290833d125ccf5401b3ab98e
Branden Archer <b.m.archer4@gmail.com>
2016-01-23 20:12:45 -0500

    Add options menu for transactions

--- app/src/main/java/protect/budgetwatch/TransactionActivity.java
+++ app/src/main/java/protect/budgetwatch/TransactionActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TransactionActivity extends AppCompatActivity
 {
@@ -65,6 +67,20 @@ public class TransactionActivity extends AppCompatActivity
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        return super.onOptionsItemSelected(item);
     }
 }
 
