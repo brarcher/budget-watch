f1959af57e0b650f513a7f4ab84a87fa380d1263
Branden Archer <b.m.archer4@gmail.com>
2016-01-28 00:56:37 -0500

    Move TransactionPagerAdapter to its own file
    This way the class can be more easily unit tested

--- app/src/main/java/protect/budgetwatch/TransactionActivity.java
+++ app/src/main/java/protect/budgetwatch/TransactionActivity.java
@@ -110,34 +110,3 @@ public class TransactionActivity extends AppCompatActivity
         }
     }
 }
-
-class TransactionPagerAdapter extends FragmentStatePagerAdapter
-{
-    final int numTabs;
-
-    public TransactionPagerAdapter(FragmentManager fm, int numTabs)
-    {
-        super(fm);
-        this.numTabs = numTabs;
-    }
-
-    @Override
-    public Fragment getItem(int position)
-    {
-        switch (position)
-        {
-            case 0:
-                return new TransactionExpenseFragment();
-            case 1:
-                return new TransactionRevenueFragment();
-            default:
-                return null;
-        }
-    }
-
-    @Override
-    public int getCount()
-    {
-        return numTabs;
-    }
-}
