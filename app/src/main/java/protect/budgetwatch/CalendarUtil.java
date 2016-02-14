112cbc4c1ec5bb699017bd05d1cabed69fa49b42
Branden Archer <b.m.archer4@gmail.com>
2016-02-13 22:54:05 -0500

    Move time adjusting calls to separate class

---
+++ app/src/main/java/protect/budgetwatch/CalendarUtil.java
@@ -0,0 +1,62 @@
+package protect.budgetwatch;
+
+import java.util.Calendar;
+
+public class CalendarUtil
+{
+    public static long getStartOfMonthMs(int year, int month)
+    {
+        final Calendar date = Calendar.getInstance();
+
+        date.set(Calendar.YEAR, year);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, date.getActualMinimum(Calendar.DAY_OF_MONTH));
+        date.set(Calendar.HOUR_OF_DAY, date.getActualMinimum(Calendar.HOUR_OF_DAY));
+        date.set(Calendar.MINUTE, date.getActualMinimum(Calendar.MINUTE));
+        date.set(Calendar.SECOND, date.getActualMinimum(Calendar.SECOND));
+        date.set(Calendar.MILLISECOND, date.getActualMinimum(Calendar.MILLISECOND));
+        return date.getTimeInMillis();
+    }
+
+    public static long getEndOfMonthMs(int year, int month)
+    {
+        final Calendar date = Calendar.getInstance();
+
+        date.set(Calendar.YEAR, year);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, date.getActualMaximum(Calendar.DAY_OF_MONTH));
+        date.set(Calendar.HOUR_OF_DAY, date.getActualMaximum(Calendar.HOUR_OF_DAY));
+        date.set(Calendar.MINUTE, date.getActualMaximum(Calendar.MINUTE));
+        date.set(Calendar.SECOND, date.getActualMaximum(Calendar.SECOND));
+        date.set(Calendar.MILLISECOND, date.getActualMaximum(Calendar.MILLISECOND));
+        return date.getTimeInMillis();
+    }
+
+    public static long getStartOfDayMs(int year, int month, int day)
+    {
+        final Calendar date = Calendar.getInstance();
+
+        date.set(Calendar.YEAR, year);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, day);
+        date.set(Calendar.HOUR_OF_DAY, date.getActualMinimum(Calendar.HOUR_OF_DAY));
+        date.set(Calendar.MINUTE, date.getActualMinimum(Calendar.MINUTE));
+        date.set(Calendar.SECOND, date.getActualMinimum(Calendar.SECOND));
+        date.set(Calendar.MILLISECOND, date.getActualMinimum(Calendar.MILLISECOND));
+        return date.getTimeInMillis();
+    }
+
+    public static long getEndOfDayMs(int year, int month, int day)
+    {
+        final Calendar date = Calendar.getInstance();
+
+        date.set(Calendar.YEAR, year);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, day);
+        date.set(Calendar.HOUR_OF_DAY, date.getActualMaximum(Calendar.HOUR_OF_DAY));
+        date.set(Calendar.MINUTE, date.getActualMaximum(Calendar.MINUTE));
+        date.set(Calendar.SECOND, date.getActualMaximum(Calendar.SECOND));
+        date.set(Calendar.MILLISECOND, date.getActualMaximum(Calendar.MILLISECOND));
+        return date.getTimeInMillis();
+    }
+}
