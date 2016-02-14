220673afee4e7684a005c10b6940384b822aa26d
Branden Archer <b.m.archer4@gmail.com>
2016-02-13 23:57:49 -0500

    Inflate super class' options menu
    This is mostly for completeness.

--- app/src/main/java/protect/budgetwatch/TransactionActivity.java
+++ app/src/main/java/protect/budgetwatch/TransactionActivity.java
@@ -72,7 +72,7 @@ public class TransactionActivity extends AppCompatActivity
     {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.add_menu, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
