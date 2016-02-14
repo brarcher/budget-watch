ae5b6cbb397c8ec9b1604de8981731f035fa1703
Branden Archer <b.m.archer4@gmail.com>
2016-02-13 21:51:58 -0500

    Add a task for cleaning up loose ends in the database
    Specifically, this will currently determine the consistency
    of receipts in the database, and will either remove orphan
    receipts or update transactions that point to receipts
    that no longer exist for some reason. In addition, old
    receipts may also be purged.

---
+++ app/src/main/java/protect/budgetwatch/DatabaseCleanupTask.java
@@ -0,0 +1,168 @@
+package protect.budgetwatch;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+public class DatabaseCleanupTask  extends AsyncTask<Void, Void, Void>
+{
+    private static final String TAG = "BudgetWatch";
+
+    private final Activity activity;
+    private final Long receiptPurgeCutoff;
+
+    private ProgressDialog progress;
+
+    public DatabaseCleanupTask(Activity activity)
+    {
+        super();
+        this.activity = activity;
+        receiptPurgeCutoff = null;
+    }
+
+    public DatabaseCleanupTask(Activity activity, long receiptPurgeCutoff)
+    {
+        super();
+        this.activity = activity;
+        this.receiptPurgeCutoff = receiptPurgeCutoff;
+    }
+
+    protected void onPreExecute()
+    {
+        progress = new ProgressDialog(activity);
+        progress.setTitle(R.string.cleaning);
+
+        progress.setOnDismissListener(new DialogInterface.OnDismissListener()
+        {
+            @Override
+            public void onDismiss(DialogInterface dialog)
+            {
+                DatabaseCleanupTask.this.cancel(true);
+            }
+        });
+
+        progress.show();
+    }
+
+    private void removeOldReceiptsFromTransactions(DBHelper db)
+    {
+        Cursor cursor = db.getTransactionsWithReceipts(receiptPurgeCutoff);
+
+        while(cursor.moveToNext())
+        {
+            Transaction transaction = Transaction.toTransaction(cursor);
+            File receipt = new File(transaction.receipt);
+            boolean result = receipt.delete();
+            if(result == false)
+            {
+                Log.i(TAG, "Failed to delete old receipt from transaction: " + transaction.id);
+            }
+
+            db.updateTransaction(transaction.id, transaction.type, transaction.description,
+                    transaction.account, transaction.budget, transaction.value, transaction.note,
+                    transaction.dateMs, /* no receipt */ "");
+        }
+        cursor.close();
+    }
+
+    private void correctTransactionsWithMissingReceipts(DBHelper db)
+    {
+        Cursor cursor = db.getTransactionsWithReceipts(null);
+
+        while(cursor.moveToNext())
+        {
+            Transaction transaction = Transaction.toTransaction(cursor);
+            if(transaction.receipt.isEmpty() == false)
+            {
+                File receipt = new File(transaction.receipt);
+                if(receipt.isFile() == false)
+                {
+                    // This entry's receipt is missing. Cannot recover
+                    // the receipt image, but can update database to remove
+                    // the receipt from the transaction
+                    db.updateTransaction(transaction.id, transaction.type, transaction.description,
+                            transaction.account, transaction.budget, transaction.value, transaction.note,
+                            transaction.dateMs, /* no receipt */ "");
+                    Log.i(TAG, "Transaction " + transaction.id + " listed a receipt but it is missing, " +
+                            "removing receipt");
+                }
+            }
+        }
+        cursor.close();
+    }
+
+    private void deleteOrphanedReceipts(DBHelper db)
+    {
+        File imageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if(imageDir == null || imageDir.exists() == false)
+        {
+            // There are no images to cleanup
+            return;
+        }
+
+        Cursor cursor = db.getTransactionsWithReceipts(null);
+
+        for(File receipt : imageDir.listFiles())
+        {
+            // Search for this receipt attached to a transaction
+            boolean found = false;
+            cursor.moveToPosition(-1);
+            while(cursor.moveToNext())
+            {
+                Transaction transaction = Transaction.toTransaction(cursor);
+                File transactionReceipt = new File(transaction.receipt);
+                if(transactionReceipt.equals(receipt))
+                {
+                    // Found the receipt used in the database, ok to move on
+                    found = true;
+                    break;
+                }
+            }
+
+            if(found == false)
+            {
+                Log.i(TAG, "Deleting orphaned receipt: " + receipt.getAbsolutePath());
+                boolean result = receipt.delete();
+                if(result == false)
+                {
+                    Log.w(TAG, "Failed to delete orphaned receipt: " + receipt.getAbsolutePath());
+                }
+            }
+        }
+
+        cursor.close();
+    }
+
+    protected Void doInBackground(Void... nothing)
+    {
+        DBHelper db = new DBHelper(activity);
+
+        if(receiptPurgeCutoff != null)
+        {
+            removeOldReceiptsFromTransactions(db);
+        }
+
+        correctTransactionsWithMissingReceipts(db);
+        deleteOrphanedReceipts(db);
+
+        return null;
+    }
+
+    protected void onPostExecute(Void result)
+    {
+        progress.dismiss();
+        Log.i(TAG, "Cleanup Complete");
+    }
+
+    protected void onCancelled()
+    {
+        progress.dismiss();
+        Log.i(TAG, "Cleanup Cancelled");
+    }
+}
---
+++ app/src/test/java/protect/budgetwatch/DatabaseCleanupTaskTest.java
@@ -0,0 +1,171 @@
+package protect.budgetwatch;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Environment;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 17)
+public class DatabaseCleanupTaskTest
+{
+    private Activity activity;
+    private DBHelper db;
+    File imageDir;
+    File missingReceipt;
+    File orphanReceipt;
+
+    private final int NUM_TRANSACTIONS = 10;
+    private final String WITH_RECEIPT_NAME = "receipt_exists";
+    private final String WITHOUT_RECEIPT_NAME = "receipt_missing";
+
+    @Before
+    public void setUp() throws IOException
+    {
+        activity = Robolectric.setupActivity(ImportExportActivity.class);
+        db = new DBHelper(activity);
+
+        imageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        assertNotNull(imageDir);
+
+        boolean result;
+        if(imageDir.exists() == false)
+        {
+            result = imageDir.mkdirs();
+            assertTrue(result);
+        }
+
+        missingReceipt = new File(imageDir, "missing");
+
+        orphanReceipt = new File(imageDir, "orphan");
+        result = orphanReceipt.createNewFile();
+        assertTrue(result);
+    }
+
+    /**
+     * Add 10 transactions each of the following:
+     *  - expense
+     *    * receipt exists
+     *    * receipt missing
+     *  - revenue
+     *    * receipt exists
+     *    * receipt missing
+     */
+    private void addTransactions() throws IOException
+    {
+        for(int type : new Integer[]{DBHelper.TransactionDbIds.REVENUE, DBHelper.TransactionDbIds.EXPENSE})
+        {
+            for(int index = 1; index <= NUM_TRANSACTIONS; index++)
+            {
+                File existingReceipt = new File(imageDir, "exists-" + type + "-" + index);
+                boolean result = existingReceipt.createNewFile();
+                assertTrue(result);
+
+                result = db.insertTransaction(type,
+                        WITH_RECEIPT_NAME,
+                        DBHelper.TransactionDbIds.ACCOUNT,
+                        DBHelper.TransactionDbIds.BUDGET,
+                        0,
+                        DBHelper.TransactionDbIds.NOTE,
+                        index,
+                        existingReceipt.getAbsolutePath());
+                assertTrue(result);
+
+                result = db.insertTransaction(type,
+                        WITHOUT_RECEIPT_NAME,
+                        DBHelper.TransactionDbIds.ACCOUNT,
+                        DBHelper.TransactionDbIds.BUDGET,
+                        0,
+                        DBHelper.TransactionDbIds.NOTE,
+                        index,
+                        missingReceipt.getAbsolutePath());
+                assertTrue(result);
+            }
+        }
+    }
+
+    @Test
+    public void testCleanupOnly() throws IOException
+    {
+        addTransactions();
+
+        DatabaseCleanupTask task = new DatabaseCleanupTask(activity);
+        task.execute();
+
+        // Actually run the task to completion
+        Robolectric.flushBackgroundThreadScheduler();
+
+        // Check that the orphaned image is now deleted
+        assertEquals(false, orphanReceipt.exists());
+
+        // Check that the database only reports transactions with
+        // existing receipts
+        Cursor cursor = db.getTransactionsWithReceipts(null);
+
+        // There should be NUM_TRANSACTIONS transactions for each of
+        // REVENUE and EXPENSE
+        assertEquals(NUM_TRANSACTIONS*2, cursor.getCount());
+
+        while(cursor.moveToNext())
+        {
+            Transaction transaction = Transaction.toTransaction(cursor);
+            assertEquals(WITH_RECEIPT_NAME, transaction.description);
+
+            // Check that the image still exists
+            File receipt = new File(transaction.receipt);
+            assertEquals(true, receipt.exists());
+            assertEquals(true, receipt.isFile());
+        }
+    }
+
+    @Test
+    public void testCleanupAndPurge() throws IOException
+    {
+        addTransactions();
+
+        final Long DATE_CUTOFF = (long)NUM_TRANSACTIONS/2;
+
+        DatabaseCleanupTask task = new DatabaseCleanupTask(activity, DATE_CUTOFF);
+        task.execute();
+
+        // Actually run the task to completion
+        Robolectric.flushBackgroundThreadScheduler();
+
+        // Check that the orphaned image is not deleted
+        assertEquals(false, orphanReceipt.exists());
+
+        // Check that the database only reports transactions with
+        // existing receipts
+        Cursor cursor = db.getTransactionsWithReceipts(null);
+
+        // Before the purge there were NUM_TRANSACTIONS transactions for each of
+        // REVENUE and EXPENSE. The purge will reduce the count by half.
+        assertEquals(NUM_TRANSACTIONS, cursor.getCount());
+
+        while(cursor.moveToNext())
+        {
+            Transaction transaction = Transaction.toTransaction(cursor);
+            assertEquals(WITH_RECEIPT_NAME, transaction.description);
+
+            // Check that the image still exists
+            File receipt = new File(transaction.receipt);
+            assertEquals(true, receipt.exists());
+            assertEquals(true, receipt.isFile());
+
+            assertTrue(transaction.dateMs > DATE_CUTOFF);
+        }
+    }
+}
