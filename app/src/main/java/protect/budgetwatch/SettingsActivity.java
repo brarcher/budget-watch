87b42f06dc82bbdc58b9bc941a597f72a9fc086d
Branden Archer <b.m.archer4@gmail.com>
2016-07-14 09:17:49 -0400

    Allow user to configure jpeg quality for receipt images
    Previously the receipt images were saved at whatever jpeg quality
    was used on the images from the camera. This quality may be much
    higher than is needed to store a readable receipt image.
    
    To prevent receipts from taking up too much space, this commit
    adds a setting for the jpeg quality to use for receipts. If set
    to less than 100, the receipt image is re-encoded before being saved.
    The default quality used is 40, which empirically seems to be more
    than sufficient for representing a receipt with minimal image
    artifacts.

---
+++ app/src/main/java/protect/budgetwatch/SettingsActivity.java
@@ -0,0 +1,41 @@
+package protect.budgetwatch;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
+}
