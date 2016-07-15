14255a77738183231bb9f33ec2006b33949cf3a4
Branden Archer <b.m.archer4@gmail.com>
2016-07-15 18:29:44 -0400

    Always close CSVPrinter when finished
    It was previously possible to hit an IOException when using the
    CSVPrinter, which would result in the exception leaving exportData()
    without closing the CSVPrinter. This changes wraps the usage
    of the CSVPrinter in a try/finally block so that it may
    be closed even if an IOException is thrown.

--- app/src/main/java/protect/budgetwatch/CsvDatabaseExporter.java
+++ app/src/main/java/protect/budgetwatch/CsvDatabaseExporter.java
@@ -18,61 +18,67 @@ public class CsvDatabaseExporter implements DatabaseExporter
     {
         CSVPrinter printer = new CSVPrinter(output, CSVFormat.RFC4180);
 
-        // Print the header
-        printer.printRecord(DBHelper.TransactionDbIds.NAME,
-                DBHelper.TransactionDbIds.TYPE,
-                DBHelper.TransactionDbIds.DESCRIPTION,
-                DBHelper.TransactionDbIds.ACCOUNT,
-                DBHelper.TransactionDbIds.BUDGET,
-                DBHelper.TransactionDbIds.VALUE,
-                DBHelper.TransactionDbIds.NOTE,
-                DBHelper.TransactionDbIds.DATE);
-
-        for(Cursor cursor : new Cursor[]{db.getExpenses(), db.getRevenues()})
+        try
         {
-            while(cursor.moveToNext())
+
+            // Print the header
+            printer.printRecord(DBHelper.TransactionDbIds.NAME,
+                    DBHelper.TransactionDbIds.TYPE,
+                    DBHelper.TransactionDbIds.DESCRIPTION,
+                    DBHelper.TransactionDbIds.ACCOUNT,
+                    DBHelper.TransactionDbIds.BUDGET,
+                    DBHelper.TransactionDbIds.VALUE,
+                    DBHelper.TransactionDbIds.NOTE,
+                    DBHelper.TransactionDbIds.DATE);
+
+            for (Cursor cursor : new Cursor[]{db.getExpenses(), db.getRevenues()})
             {
-                Transaction transaction = Transaction.toTransaction(cursor);
+                while (cursor.moveToNext())
+                {
+                    Transaction transaction = Transaction.toTransaction(cursor);
 
-                printer.printRecord(transaction.id,
-                        transaction.type == DBHelper.TransactionDbIds.EXPENSE ?
-                                "EXPENSE" : "REVENUE",
-                        transaction.description,
-                        transaction.account,
-                        transaction.budget,
-                        transaction.value,
-                        transaction.note,
-                        transaction.dateMs);
+                    printer.printRecord(transaction.id,
+                            transaction.type == DBHelper.TransactionDbIds.EXPENSE ?
+                                    "EXPENSE" : "REVENUE",
+                            transaction.description,
+                            transaction.account,
+                            transaction.budget,
+                            transaction.value,
+                            transaction.note,
+                            transaction.dateMs);
 
-                if(Thread.currentThread().isInterrupted())
+                    if (Thread.currentThread().isInterrupted())
+                    {
+                        throw new InterruptedException();
+                    }
+                }
+
+                cursor.close();
+            }
+
+
+            for (String budgetName : db.getBudgetNames())
+            {
+                Budget budget = db.getBudgetStoredOnly(budgetName);
+
+                printer.printRecord(budget.name,
+                        "BUDGET",
+                        "", // blank description
+                        "", // blank account
+                        "", // blank budget (handled in id field)
+                        budget.max,
+                        "", // blank note
+                        ""); // blank date
+
+                if (Thread.currentThread().isInterrupted())
                 {
                     throw new InterruptedException();
                 }
             }
-
-            cursor.close();
         }
-
-
-        for(String budgetName : db.getBudgetNames())
+        finally
         {
-            Budget budget = db.getBudgetStoredOnly(budgetName);
-
-            printer.printRecord(budget.name,
-                    "BUDGET",
-                    "", // blank description
-                    "", // blank account
-                    "", // blank budget (handled in id field)
-                    budget.max,
-                    "", // blank note
-                    ""); // blank date
-
-            if(Thread.currentThread().isInterrupted())
-            {
-                throw new InterruptedException();
-            }
+            printer.close();
         }
-
-        printer.close();
     }
 }
