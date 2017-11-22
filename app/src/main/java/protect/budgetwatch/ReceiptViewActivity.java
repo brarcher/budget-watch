0dcb52c2034d59d3a40f83a4eeca9ccd3950e6a1
Branden Archer <b.m.archer4@gmail.com>
2017-11-22 15:28:21 -0500

    Export receipt images using FileProvider
    Starting in Android 24 file Uris cannot be used
    outside of an Activity. To allow images to be
    captured with a camera, a content Uri needs to be used.
    This change will use a FileProvider on Android 24+.
    For earlier versions the file Uri will still be used.
    This provides backwards compatability, as well as allows
    Robolectric to continue to test the general code path.

--- app/src/main/java/protect/budgetwatch/ReceiptViewActivity.java
+++ app/src/main/java/protect/budgetwatch/ReceiptViewActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -87,7 +88,12 @@ public class ReceiptViewActivity  extends AppCompatActivity
         BitmapFactory.decodeFile(receiptFilename, opt);
         shareIntent.setType(opt.outMimeType);
 
-        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(receiptFilename)));
+        Uri outputUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, new File(receiptFilename));
+        shareIntent.putExtra(Intent.EXTRA_STREAM, outputUri);
+
+        // set flag to give temporary permission to external app to use the FileProvider
+        shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
         shareActionProvider.setShareIntent(shareIntent);
 
         return super.onCreateOptionsMenu(menu);
